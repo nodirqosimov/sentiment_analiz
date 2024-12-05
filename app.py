dd593343a0fb6132b6583053744958d27431915e
Nodir <qosimovn987@gmail.com>
2024-12-05 15:52:58 +0500

    app

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from Flask inside Streamlit!"
+    return "Hello from Flask!"
 
+# Ensure Flask doesn't run within Streamlit
 if __name__ == "__main__":
-    # Flask serverni Streamlitda ishga tushirmang!
-    pass
-
+    app.run(debug=True)
